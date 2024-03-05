1d48428475bb97c44261bd21b69fe4272dde477d
EmilianoVR <emiliano_velazquez@hotmail.com>
2024-03-04 20:38:02 -0600

    Problemas con server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 ﻿const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('PlataformaProyectos'));
+app.use(express.static(path.join(__dirname, 'PlataformaProyectos')));
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
