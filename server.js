89d60a19a0321475a3c18d7c7a56b38d81774b56
EmilianoVR <emiliano_velazquez@hotmail.com>
2024-03-04 19:50:11 -0600

    Mas back

---
+++ server.js
@@ -0,0 +1,9 @@
+﻿const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
