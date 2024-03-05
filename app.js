fa3bad6836090db4684426d5aaa93e21530671c3
EmilianoVR <emiliano_velazquez@hotmail.com>
2024-03-04 19:09:00 -0600

    Primer Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const formCrearProyecto = document.getElementById('form-crear-proyecto');
+    const listaProyectos = document.getElementById('lista-proyectos');
+
+    formCrearProyecto.addEventListener('submit', function (event) {
+        event.preventDefault();
+        
+        // Obtener el nombre del proyecto ingresado por el usuario
+        const nombreProyecto = document.getElementById('nombre-proyecto').value;
+
+        // Crear un nuevo elemento de lista para mostrar el proyecto
+        const nuevoProyecto = document.createElement('li');
+        nuevoProyecto.textContent = nombreProyecto;
+
+        // Agregar el nuevo proyecto a la lista de proyectos
+        listaProyectos.appendChild(nuevoProyecto);
+
+        // Limpiar el campo de entrada después de agregar el proyecto
+        document.getElementById('nombre-proyecto').value = '';
+    });
+});
