3af3cd7d591a8824fc77f43d654e0778797e8dc3
EmilianoVR <emiliano_velazquez@hotmail.com>
2024-03-04 20:45:04 -0600

    fg56g

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 ﻿const express = require('express');
 const app = express();
-const path = require('path');
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'PlataformaProyectos')));
+app.use(express.static('Paquetes'));
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
